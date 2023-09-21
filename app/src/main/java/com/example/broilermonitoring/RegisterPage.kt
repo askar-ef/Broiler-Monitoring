120e00fb4c5d692ed803af61859ae2e648377639
IlhamRafi73 <ilhamrafimahameru2004@mail.ugm.ac.id>
2023-09-21 12:55:38 +0700

    Homepage

--- app/src/main/java/com/example/broilermonitoring/RegisterPage.kt
+++ app/src/main/java/com/example/broilermonitoring/RegisterPage.kt
@@ -3,9 +3,9 @@ package com.example.broilermonitoring
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegisterPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register_page)
     }
 }
