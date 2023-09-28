66a30c82acc02a89042da05f5ede58992c3b2760
Harits <haritsirawan2118@gmail.com>
2023-09-28 13:27:17 +0700

    28 September

---
+++ app/src/main/java/com/example/broilermonitoring/AkunActivity.kt
@@ -0,0 +1,11 @@
+package com.example.broilermonitoring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AkunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.akun)
+    }
+}
--- app/src/main/java/com/example/broilermonitoring/HomePageActivity.kt
+++ app/src/main/java/com/example/broilermonitoring/HomePageActivity.kt
@@ -2,10 +2,21 @@ package com.example.broilermonitoring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page)
+        val spinner = findViewById<Spinner>(R.id.kandang) // Replace with your Spinner's ID
+        val customAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.kandang, // Replace with your Spinner's item array resource
+            R.layout.spinner_item // Use the custom layout you created
+        )
+
+        customAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = customAdapter
     }
 }
